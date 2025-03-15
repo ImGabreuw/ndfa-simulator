04859589c3b6832501e8eb4f32bd3527cd7de4fb
ImGabreuw <imgabreuw.dev@gmail.com>
2025-03-15 16:11:31 -0300

    test: add new test case to read transition function

--- test/reader-test.c
+++ test/reader-test.c
@@ -24,11 +24,34 @@ int test_read_first_line_to_get_automaton_alphabet_symbols(void)
     return failures;
 }
 
+int test_read_sixth_line_to_get_automaton_first_transition_function(void)
+{
+    const char input_file[] = "in-test.txt";
+
+    FILE *file = fopen(input_file, "r");
+    if (file == NULL)
+    {
+        printf("Erro ao abrir o arquivo.\n");
+        return EXIT_FAILURE;
+    }
+
+    char first_transition_function[MAX_LINE_SIZE];
+    read_line(file, 6, first_transition_function);
+
+    int failures = 0;
+    failures += assertStringEquals("0 a 0", first_transition_function, NULL);
+    return failures;
+}
+
 int main(void)
 {
     run_test(
         "Should read first line to get the automaton alphabet symbols",
         &test_read_first_line_to_get_automaton_alphabet_symbols);
+
+    run_test(
+        "Should read sixth line to get automaton first transition function",
+        &test_read_sixth_line_to_get_automaton_first_transition_function);
 
     return EXIT_SUCCESS;
 }
