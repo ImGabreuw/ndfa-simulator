c8f111cfe5d18aef7c9b831b50322d8447e34a88
ImGabreuw <imgabreuw.dev@gmail.com>
2025-03-15 19:29:47 -0300

    test: end word with a state that do not have transition function

--- src/main.c
+++ src/main.c
@@ -24,16 +24,15 @@ int main(void)
        q3 --'d'--> q4
     */
     set_transition(&automaton, 0, 'a', 1);
-    set_transition(&automaton, 0, 'a', 2); // Outra transição para 'a' a partir de q0
+    set_transition(&automaton, 0, 'a', 2);
     set_transition(&automaton, 1, 'b', 3);
     set_transition(&automaton, 2, 'c', 3);
     set_transition(&automaton, 3, 'd', 4);
 
-    // Definir os estados finais (exemplo: apenas q4 é final)
     automaton.num_final_states = 1;
     automaton.final_states[0] = 4;
 
-    const char *word1 = "abd"; // Pode ser rejeitada se não houver caminho completo
+    const char *word1 = "abdc"; // Caminho: q0->(a)q2->(b)q3->(d)q4->(c)q4 => Aceita (não tem caminho a partir de q4, ignora)
     const char *word2 = "acd"; // Caminho: q0->(a)q2->(c)q3->(d)q4 => Aceita
 
     int res1 = check(&automaton, word1);
