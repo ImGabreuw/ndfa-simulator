e38c108371d0fb8d3e85a4f105134bb2768a8ef1
ImGabreuw <imgabreuw.dev@gmail.com>
2025-03-15 16:06:14 -0300

    fix: ignoring line break when read file line

--- src/reader.c
+++ src/reader.c
@@ -6,7 +6,8 @@ const int MAX_LINE_SIZE = 100;
 
 void read_line(FILE *file, int line_index, char line[MAX_LINE_SIZE])
 {
-    if (file == NULL) {
+    if (file == NULL)
+    {
         printf("Erro ao ler o arquivo.\n");
         return;
     }
@@ -14,15 +15,22 @@ void read_line(FILE *file, int line_index, char line[MAX_LINE_SIZE])
     char buffer[MAX_LINE_SIZE];
     int current_line = 1;
 
-    while (fgets(buffer, MAX_LINE_SIZE, file)) {
-        if (current_line == line_index) {
+    while (fgets(buffer, MAX_LINE_SIZE, file))
+    {
+        if (current_line == line_index)
+        {
+            if (strlen(buffer) <= 0)
+                break;
+
+            buffer[strlen(buffer) - 1] = 0;
             strcpy(line, buffer);
             break;
         }
         current_line++;
     }
 
-    if (current_line < line_index) {
+    if (current_line < line_index)
+    {
         printf("O arquivo tem menos de %d linhas.\n", line_index);
     }
 }
