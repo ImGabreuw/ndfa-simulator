858d2f646971e91178b7886506dc4c59f8c7dee9
ImGabreuw <imgabreuw.dev@gmail.com>
2025-03-15 15:59:07 -0300

    feat: function to read specific line

---
+++ include/models.h
@@ -0,0 +1,2 @@
+// abordagem moderna em relação ao #ifdef para reduzir a chance de erros de múltipla inclusão.
+#pragma once
---
+++ src/reader.c
@@ -0,0 +1,28 @@
+#include "reader.h"
+
+#include <string.h>
+
+const int MAX_LINE_SIZE = 100;
+
+void read_line(FILE *file, int line_index, char line[MAX_LINE_SIZE])
+{
+    if (file == NULL) {
+        printf("Erro ao ler o arquivo.\n");
+        return;
+    }
+
+    char buffer[MAX_LINE_SIZE];
+    int current_line = 1;
+
+    while (fgets(buffer, MAX_LINE_SIZE, file)) {
+        if (current_line == line_index) {
+            strcpy(line, buffer);
+            break;
+        }
+        current_line++;
+    }
+
+    if (current_line < line_index) {
+        printf("O arquivo tem menos de %d linhas.\n", line_index);
+    }
+}
